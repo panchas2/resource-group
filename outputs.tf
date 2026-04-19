5e80afc3f751093b6aea0126207efe608030d3db
panchas2 <sankaran.coin@gmail.com>
2026-04-19 05:34:44 +0530

    Add files via upload
    main op var files added

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg-name.name
+}
+
+output "location" {
+  value = azurerm_resource_group.rg-name.location
+}
+
+output "resource_group_id" {
+  value = azurerm_resource_group.rg-name.id
+}
